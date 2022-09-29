da26af7123c2cbcec045806573d28535a925da94
Fonzy Fermin <50535575+fonzy1243@users.noreply.github.com>
2022-09-29 17:15:58 +0800

    Added code discussed on 9/29/2022.

--- IO_Test.c
+++ IO_Test.c
@@ -2,27 +2,33 @@
 
 int main()
 {
-	int x = 5;
-	int y = 10;
+	// int x = 5;
+	// int y = 10;
 
-	printf("%d * %d = %d\n\n", x, y, x * y);
+	// printf("%d * %d = %d\n\n", x, y, x * y);
 
 	int nNum;
 	float fNum;
 	char cChar;
+	char cTemp;
 
-	printf("Enter a number: ");
-	scanf("%d", &nNum);
-	printf("Input value is: %d", nNum);
+	// printf("Enter a number: ");
+	// scanf("%d", &nNum);
+	// printf("Input value is: %d", nNum);
 
-	printf("\n\nEnter a float: ");
-	scanf("%f", &fNum);
-	printf("Input value is: %f", fNum);
+	// printf("\n\nEnter a float: ");
+	// scanf("%f", &fNum);
+	// printf("Input value is: %f", fNum);
 
-	printf("\n\nEnter a char: ");
-	scanf("%c", &cChar);
-	scanf("%c", &cChar);
-	printf("Input value is: %c\n", cChar);
+	// printf("\n\nEnter a char: ");
+	// scanf("%c", &cChar);
+	// scanf("%c", &cChar);
+	// printf("Input value is: %c\n", cChar);
+
+	printf("Enter input: ");
+	scanf("%d%f\t%c", &nNum, &fNum, &cChar);
+	scanf("%c", &cTemp);
+	printf("Output: %d\t%f\t%c", nNum, fNum, cChar);
 
 	return 0;
 }
