e8476a6a08eda57eef8d38513bd3ec3e01da21d3
Fonzy Fermin <fonzy.fermin@gmail.com>
2022-10-03 17:50:10 +0800

    Added 10/3 lecture

--- userDefinedFunctions.c
+++ userDefinedFunctions.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
-int computeAverage(int x, int y)
+float computeAverage(float grade1, float grade2)
 {
-    printf("Average of %d and %d is: %d", x, y, (x + y) / 2);
-    return 0;
+    float average = (grade1 + grade2) / 2;
+
+    return average;
 }
 
+void displayAverage(float average)
+{
+    printf("Hands-on Exams: %.2f\n", average);
+}
 int main()
 {
-    computeAverage(3,5);
+    float handsOn1,
+        handsOn2,
+        average;
+
+    printf("Enter grade HO1: ");
+    scanf("%f", &handsOn1);
+    printf("Enter grade HO2: ");
+    scanf("%f", &handsOn2);
+
+    average = computeAverage(handsOn1, handsOn2);
+    displayAverage(average);
+
+    printf("Enter grade HO1: ");
+    scanf("%f", &handsOn1);
+    printf("Enter grade HO2: ");
+    scanf("%f", &handsOn2);
+
+    average = computeAverage(handsOn1, handsOn2);
+    displayAverage(average);
+
     return 0;
 }
-
